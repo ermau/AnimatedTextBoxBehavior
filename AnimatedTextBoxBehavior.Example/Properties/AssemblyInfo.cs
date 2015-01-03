e9d86c93c56e720bb99c6793ecc95d61027b8866
Eric Maupin <me@ermau.com>
2015-01-03 15:35:01 -0500

    Update sample a bit

--- AnimatedTextBoxBehavior.Example/Properties/AssemblyInfo.cs
+++ AnimatedTextBoxBehavior.Example/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AnimatedTextBoxBehavior.Example")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Xamarin Inc. 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
