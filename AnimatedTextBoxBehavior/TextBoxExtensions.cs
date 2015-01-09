d4f00d8921721e767282e606000186acd0be2399
Eric Maupin <me@ermau.com>
2015-01-09 00:25:07 -0500

    Add TextBox.GetRectFromLineIndex extension

---
+++ AnimatedTextBoxBehavior/TextBoxExtensions.cs
@@ -0,0 +1,45 @@
+﻿//
+// TextBoxExtensions.cs
+//
+// Author:
+//   Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2015 Xamarin Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AnimatedTextBoxBehavior
+{
+	internal static class TextBoxExtensions
+	{
+		public static Rect GetRectFromLineIndex (this TextBox self, int lineIndex)
+		{
+			int startChar = self.GetCharacterIndexFromLineIndex (lineIndex);
+			int lastChar = self.GetLineLength (lineIndex) + startChar - 1;
+			Rect startRect = self.GetRectFromCharacterIndex (startChar, trailingEdge: false);
+			Rect endRect = self.GetRectFromCharacterIndex (lastChar, trailingEdge: true);
+
+			return new Rect (startRect.X, startRect.Y, startRect.X + endRect.X, startRect.Height);
+		}
+	}
+}
